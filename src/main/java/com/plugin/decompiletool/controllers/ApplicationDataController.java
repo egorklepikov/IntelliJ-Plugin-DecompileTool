561ecceb99792c3530bfa6b54d4f5ec6ef765b5f
egor.klepikov <Egor.Klepikov@jetbrains.com>
2020-07-04 19:28:10 +0300

    Implemented ApplicationDataController

---
+++ src/main/java/com/plugin/decompiletool/controllers/ApplicationDataController.java
@@ -0,0 +1,53 @@
+package com.plugin.decompiletool.controllers;
+
+import decompiletool.DecompileTool;
+import decompiletool.network.AppInformation;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ApplicationDataController {
+  private static ApplicationDataController instance;
+  private ArrayList<AppInformation> applications = new ArrayList<>();
+
+  private ApplicationDataController() {
+  }
+
+  public static ApplicationDataController getInstance() {
+    if (instance == null) {
+      instance = new ApplicationDataController();
+    }
+    return instance;
+  }
+
+  public int loadData(String searchQuery) {
+    applications = DecompileTool.getInstance().getAppsList(searchQuery);
+    if (applications == null) {
+      return 2;
+    } else {
+      return 3;
+    }
+  }
+
+  public HashMap<String, AppInformation> getAppsList() {
+    if (applications == null) {
+      throw new RuntimeException("Applications aren't loaded or null.");
+    }
+    HashMap<String, AppInformation> appsMap = new HashMap<>();
+    for (AppInformation application : applications) {
+      String comboboxPresentation = application.getAppName() + "\t" + "[" + application.getVendor() + "]";
+      appsMap.put(comboboxPresentation, application);
+    }
+    return appsMap;
+  }
+
+  public HashMap<String, AppInformation.AppRelease> getAppReleaseInfo(AppInformation appInformation) {
+    ArrayList<AppInformation.AppRelease> versions = appInformation.getAppVersions();
+    HashMap<String, AppInformation.AppRelease> appReleases = new HashMap<>();
+    for(AppInformation.AppRelease version : versions) {
+      String releasePresentation = "Version: " + version.getReleaseVersion() + "\t [" + version.getReleaseDate() + ", " + version.getReleaseSize() + "]";
+      appReleases.put(releasePresentation, version);
+    }
+    return appReleases;
+  }
+}
